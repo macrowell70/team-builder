79809ee651e8d9dba4b4edbb2f8f628c8fe100fa
unknown <macrowell@yahoo.com>
2022-04-26 18:50:28 -0400

    added formValues slice of state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import React, { useState } from 'react';
 import Form from './Form';
 
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: ""
+}
+
 function App() {
   const [teamMembers, setTeamMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  
 
   return (
     <div className="App">

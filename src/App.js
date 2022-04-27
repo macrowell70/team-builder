a0d144572a4acb711c1b13f1f86a9984f6f68a63
unknown <macrowell@yahoo.com>
2022-04-26 20:17:34 -0400

    added team members list

--- src/App.js
+++ src/App.js
@@ -3,23 +3,51 @@ import './App.css';
 import React, { useState } from 'react';
 import Form from './Form';
 
-const initialFormValues = {
+
+const initialValues = {
   name: "",
   email: "",
   role: ""
 }
 
-function App() {
-  const [teamMembers, setTeamMembers] = useState([]);
-  const [formValues, setFormValues] = useState(initialFormValues);
 
-  
+const teamMemberList = [
+  {name: "tm1", email: "tm1@email.com", role: "engineer"},
+  {name: "tm2", email: "tm2@email.com", role: "engineer"}
+]
+
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState(teamMemberList);
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  };
+
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <div>Team List</div>
-        <Form />
+        <Form 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+        />
+
+        {teamMembers.map((member, idx) => {
+          return <p key={idx}>Name: {member.name} Email: {member.email} Role: {member.role}</p>
+        })}
+
+
       </header>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,48 @@
 import React from 'react';
 
-const Form = () => {
+const Form = (props) => {
+   const { values, update, submit } = props
+
+   const onChange = (evt) => {
+       const name = evt.target.name;
+       const value = evt.target.value;
+       update(name, value)
+   };
+
+   const onSubmit = (evt) => {
+       evt.preventDefault();
+       submit()
+   };
+
     return (
-        <form>
+        <form className="form conatiner" onSubmit={onSubmit}>
             <label>Name
-                <input type="text" />
+                <input 
+                type="text"
+                name="name"
+                placeholder="Enter Team Member Name"
+                value={values.name}
+                onChange={onChange} 
+                maxLength="30"
+                />
             </label>
             <label>Email
-                <input type="email" />
+                <input 
+                type="email"
+                name="email"
+                placeholder="Enter Team Member Email"
+                value={values.email}
+                onChange={onChange} />
             </label>
             <label>Role
-                <select>
+                <select name="role" value={values.role} onChange={onChange}>
                     <option value="">--Select a Role--</option>
                     <option value="backend">Backend Engineer</option>
                     <option value="frontend">Frontend Engineer</option>
                     <option value="designer">Designer</option>
                 </select>
             </label>
+            <button>Submit</button>
         </form>
     )
 };
